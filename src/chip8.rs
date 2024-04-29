899dfd0aef51a4b7d777ea08b72f1a5ddbeb7bce
gdziewon <116833445+gdziewon@users.noreply.github.com>
2024-04-29 15:23:55 +0200

    Make code modular
    Divided 'execute' function into smaller ones.
    Cleaned up 'Memory::new()' function by introducing 'sprites' module.

--- src/chip8.rs
+++ src/chip8.rs
@@ -12,7 +12,7 @@ pub const DISPLAY_WIDTH: usize = 64;
 pub const DISPLAY_HEIGHT: usize = 32;
 pub const SCALE: usize = 15;
 
-const MS_DELAY: u64 = 300;
+const MS_DELAY: u64 = 100;
 
 const NUM_REGISTERS: usize = 8;
 const STACK_DEPTH: usize = 16;
@@ -74,251 +74,288 @@ pub fn new() -> Self {
     }
 }
 
-fn execute( &mut self, op_code: u16, mem: &mut Memory, window: &mut Window) {
-    match op_code >> 12 { // match by most significant 4bits
+fn execute_0nnn( &mut self, op_code: u16) {
+    match op_code {
+        0x00ee => { // 00EE - RET
+            self.pc = self.stack[self.sp as usize];
+            self.sp -= 1;
+        }
 
-        0x0 => match op_code { // Starts with 0
+        0x00e0 => { // 00E0 - CLS
+            self.display = [[false; DISPLAY_HEIGHT]; DISPLAY_WIDTH];
+        }
+        _ => () // NOP
+    }
+    self.pc += 2;
+}
 
-            0x00ee => { // 00EE - RET
-                self.pc = self.stack[self.sp as usize];
-                self.sp -= 1;
+fn execute_1nnn( &mut self, op_code: u16) { // 1nnn - JP addr
+    let addr = Chip8::get_addr(op_code);
+    self.pc = addr;
+}
+
+fn execute_2nnn( &mut self, op_code: u16) { // 2nnn - CALL addr 
+    self.sp += 1;
+    self.stack[self.sp as usize] = self.pc;
+    let addr = Chip8::get_addr(op_code);
+    self.pc = addr;
+}
+
+fn execute_3xkk( &mut self, op_code: u16) { // 3xkk - SE Vx, byte
+    let vx = Chip8::get_vx(op_code);
+    let data = Chip8::get_data_byte(op_code);
+    if self.v[vx] == data {
+        self.pc += 2;
+    }
+    self.pc += 2;
+}
+
+fn execute_4xkk( &mut self, op_code: u16) { // 4xkk - SNE Vx, byte
+    let vx = Chip8::get_vx(op_code);
+    let data = Chip8::get_data_byte(op_code);
+    if self.v[vx] != data {
+        self.pc += 2;
+    }
+    self.pc += 2;
+}
+
+fn execute_5xy0( &mut self, op_code: u16) { // 5xy0 - SE Vx, Vy
+    if op_code & 0xf == 0 { 
+        let vx = Chip8::get_vx(op_code);
+        let vy = Chip8::get_vy(op_code);
+        if self.v[vx] == self.v[vy] {
+            self.pc += 2;
+        }
+    }
+    self.pc += 2;
+}
+
+fn execute_6xkk( &mut self, op_code: u16) { // 6xkk - LD Vx, byte
+    let vx = Chip8::get_vx(op_code);
+    let data = Chip8::get_data_byte(op_code);
+    self.v[vx] = data;
+    self.pc += 2;
+}
+
+fn execute_7xkk( &mut self, op_code: u16) { // 7xkk - ADD Vx, byte
+    let vx = Chip8::get_vx(op_code);
+    let data = Chip8::get_data_byte(op_code);
+    self.v[vx] = self.v[vx].wrapping_add(data);
+    self.pc += 2;
+}
+
+fn execute_8nnn( &mut self, op_code: u16) { // Start with 8
+    let vx = Chip8::get_vx(op_code);
+    let vy = Chip8::get_vy(op_code);
+    match op_code & 0xf {
+
+        0x0 => { // 8xy0 - LD Vx, Vy
+            self.v[vx] = self.v[vy];
+        }
+
+        0x1 => { // 8xy1 - OR Vx, Vy
+            self.v[vx] |= self.v[vy];
+        }
+
+        0x2 => { // 8xy2 - AND Vx, Vy
+            self.v[vx] &= self.v[vy];
+        } 
+
+        0x3 => { // 8xy3 - XOR Vx, Vy
+            self.v[vx] ^= self.v[vy];
+        }
+        
+        0x4 => { // 8xy4 - ADD Vx, Vy
+            self.v[7] = if self.v[vx] as u16 + self.v[vy] as u16 > 0xff { 1 } else { 0 };
+            self.v[vx] = self.v[vx].wrapping_add(self.v[vy]);
+        }
+
+        0x5 => { // 8xy5 - SUB Vx, Vy
+            self.v[7] = if self.v[vx] >= self.v[vy] { 1 } else { 0 };
+            self.v[vx] = self.v[vx].wrapping_sub(self.v[vy]);
+        }
+
+        0x6 => { // 8xy6 - SHR Vx {, Vy}
+            self.v[7] = self.v[vx] & 1;
+            self.v[vx] >>= 1;
+        }
+
+        0x7 => { // 8xy7 - SUBN Vx, Vy
+            self.v[7] = if self.v[vy] >= self.v[vx] { 1 } else { 0 };
+            self.v[vx] = self.v[vy].wrapping_sub(self.v[vx]);
+        }
+
+        0xe => { // 8xyE - SHL Vx {, Vy}
+            self.v[7] = self.v[vx] >> 7;
+            self.v[vx] <<= 1;
+        }
+        _ => () // NOP
+    }
+    self.pc += 2;
+}
+
+fn execute_9xy0( &mut self, op_code: u16) { // 9xy0 SNE Vx, Vy
+    if op_code & 0xf == 0 { 
+        let vx = Chip8::get_vx(op_code);
+        let vy = Chip8::get_vy(op_code);
+        if self.v[vx] != self.v[vy] {
+            self.pc += 2;
+        }
+    }
+    self.pc += 2;
+}
+
+fn execute_Annn( &mut self, op_code: u16) { // Annn - LD I, addr
+    let addr = Chip8::get_addr(op_code);
+    self.idx = addr;
+    self.pc += 2;
+}
+
+fn execute_Bnnn( &mut self, op_code: u16) { // Bnnn - JP V0, addr
+    let addr = Chip8::get_addr(op_code);
+    self.pc = addr + self.v[0] as u16;
+}
+
+fn execute_Cxkk( &mut self, op_code: u16) { // Cxkk - RND Vx, byte
+    let vx = Chip8::get_vx(op_code);
+    let data = Chip8::get_data_byte(op_code);
+    let rnd: u8 = rand::thread_rng().gen();
+    self.v[vx] = data & rnd;
+    self.pc += 2;
+}
+
+fn execute_Dxyn( &mut self, op_code: u16, mem: &Memory) { // Dxyn - DRW Vx, Vy, nibble
+    let vx = Chip8::get_vx(op_code) as usize;
+    let vy = Chip8::get_vy(op_code) as usize;
+    let height = op_code & 0xF;
+
+    self.v[7] = 0; // Reset collision register
+
+    for offset in 0..height {
+        let sprite_byte = mem.get_byte(self.idx + offset);
+        for bit in 0..8 {
+            let pixel = (sprite_byte >> (7 - bit)) & 1 != 0;
+            let x = (self.v[vx] as usize + bit) % DISPLAY_WIDTH;
+            let y = (self.v[vy] as usize + offset as usize) % DISPLAY_HEIGHT;
+
+            if self.display[x][y] & pixel {
+                self.v[7] = 1; // Set collision flag
             }
 
-            0x00e0 => { // 00E0 - CLS
-                self.display = [[false; DISPLAY_HEIGHT]; DISPLAY_WIDTH];
-            }
-            _ => () // NOP
+            // XOR pixel
+            self.display[x][y] ^= pixel;
         }
+    }
+    self.idx += height;
+    self.pc += 2;
+}
 
-        0x1 => { // 1nnn - JP addr
-            let addr = Chip8::get_addr(op_code);
-            self.pc = addr;
-            return;
-        }
-
-        0x2 => { // 2nnn - CALL addr
-            self.sp += 1;
-            self.stack[self.sp as usize] = self.pc;
-            let addr = Chip8::get_addr(op_code);
-            self.pc = addr;
-            return;
-        }
-
-        0x3 => { // 3xkk - SE Vx, byte
-            let vx = Chip8::get_vx(op_code);
-            let data = Chip8::get_data_byte(op_code);
-            if self.v[vx] == data {
-                self.pc += 2;
-            }
-        }
-
-        0x4 => { // 4xkk - SNE Vx, byte
-            let vx = Chip8::get_vx(op_code);
-            let data = Chip8::get_data_byte(op_code);
-            if self.v[vx] != data {
-                self.pc += 2;
-            }
-        }
-
-        0x5 => { 
-            if op_code & 0xf == 0 { // 5xy0 - SE Vx, Vy
-                let vx = Chip8::get_vx(op_code);
-                let vy = Chip8::get_vy(op_code);
-                if self.v[vx] == self.v[vy] {
+fn execute_Ennn( &mut self, op_code: u16, window: &Window) { // Starts with E
+    let vx = Chip8::get_vx(op_code);
+    if let Some(key) = KEYS.get_by_left(&self.v[vx]) {
+        match op_code & 0x00ff {
+            0x9e => { // Ex9E - SKP Vx
+                if window.is_key_down(*key) {
                     self.pc += 2;
                 }
-            }
-        }
-
-        0x6 => { // 6xkk - LD Vx, byte
-            let vx = Chip8::get_vx(op_code);
-            let data = Chip8::get_data_byte(op_code);
-            self.v[vx] = data;
-        }
-
-        0x7 => { // 7xkk - ADD Vx, byte
-            let vx = Chip8::get_vx(op_code);
-            let data = Chip8::get_data_byte(op_code);
-            self.v[vx] = self.v[vx].wrapping_add(data);
-        }
-
-        0x8 => { // Starts with 8
-            let vx = Chip8::get_vx(op_code);
-            let vy = Chip8::get_vy(op_code);
-            match op_code & 0xf {
-
-                0x0 => { // 8xy0 - LD Vx, Vy
-                    self.v[vx] = self.v[vy];
-                }
-
-                0x1 => { // 8xy1 - OR Vx, Vy
-                    self.v[vx] |= self.v[vy];
-                }
-
-                0x2 => { // 8xy2 - AND Vx, Vy
-                    self.v[vx] &= self.v[vy];
-                } 
-
-                0x3 => { // 8xy3 - XOR Vx, Vy
-                    self.v[vx] ^= self.v[vy];
-                }
-                
-                0x4 => { // 8xy4 - ADD Vx, Vy
-                    self.v[7] = if self.v[vx] as u16 + self.v[vy] as u16 > 0xff { 1 } else { 0 };
-                    self.v[vx] = self.v[vx].wrapping_add(self.v[vy]);
-                }
-
-                0x5 => { // 8xy5 - SUB Vx, Vy
-                    self.v[7] = if self.v[vx] >= self.v[vy] { 1 } else { 0 };
-                    self.v[vx] = self.v[vx].wrapping_sub(self.v[vy]);
-                }
-
-                0x6 => { // 8xy6 - SHR Vx {, Vy}
-                    self.v[7] = self.v[vx] & 1;
-                    self.v[vx] >>= 1;
-                }
-
-                0x7 => { // 8xy7 - SUBN Vx, Vy
-                    self.v[7] = if self.v[vy] >= self.v[vx] { 1 } else { 0 };
-                    self.v[vx] = self.v[vy].wrapping_sub(self.v[vx]);
-                }
-
-                0xe => { // 8xyE - SHL Vx {, Vy}
-                    self.v[7] = self.v[vx] >> 7;
-                    self.v[vx] <<= 1;
-                }
-                _ => () // NOP
-            }
-        }
-
-        0x9 => {
-            if op_code & 0xf == 0 { // 9xy0 SNE Vx, Vy
-                let vx = Chip8::get_vx(op_code);
-                let vy = Chip8::get_vy(op_code);
-                if self.v[vx] != self.v[vy] {
+            },
+            0xa1 => { // ExA1 - SKNP Vx
+                if !window.is_key_down(*key) {
                     self.pc += 2;
                 }
+            },
+            _ => ()
+        }
+    }
+    self.pc += 2;
+}
+
+fn execute_Fnnn( &mut self, op_code: u16, mem: &mut Memory, window: &mut Window) { // Starts with F
+    let vx = Chip8::get_vx(op_code);
+    match op_code & 0x00ff {
+        0x07 => { // Fx07 - LD Vx, DT
+            self.v[vx] = self.dt;
+        }
+
+        0x0a => { // Fx0A - LD Vx, K
+            let mut key_pressed = false;
+            while !key_pressed {
+                // Wait for key press
+                if let Some(key) = window.get_keys_pressed(KeyRepeat::No).get(0) {
+                    if let Some(chip8_key) = KEYS.get_by_right(key) {
+                        self.v[vx] = *chip8_key;
+                        key_pressed = true;
+                    }
+                }
+
+                // Keep window responsive
+                window.update();
+                std::thread::sleep(std::time::Duration::from_millis(5));
             }
         }
 
-        0xa => { // Annn - LD I, addr
-            let addr = Chip8::get_addr(op_code);
-            self.idx = addr;
+        0x15 => { // Fx15 - LD DT, Vx
+            self.dt = self.v[vx];
         }
 
-        0xb => { // Bnnn - JP V0, addr
-            let addr = Chip8::get_addr(op_code);
-            self.pc = addr + self.v[0] as u16;
-            return;
+        0x18 => { // Fx18 - LD ST, Vx
+            self.st = self.v[vx];
         }
 
-        0xc => { // Cxkk - RND Vx, byte
-            let vx = Chip8::get_vx(op_code);
-            let data = Chip8::get_data_byte(op_code);
-            let rnd: u8 = rand::thread_rng().gen();
-            self.v[vx] = data & rnd;
+        0x1e => { // Fx1E - ADD I, Vx
+            self.idx += self.v[vx] as u16;
         }
 
-        0xd => { // Dxyn - DRW Vx, Vy, nibble
-            let vx = Chip8::get_vx(op_code) as usize;
-            let vy = Chip8::get_vy(op_code) as usize;
-            let height = op_code & 0xF;
+        0x29 => { // Fx29 - LD F, Vx
+            self.idx = self.v[vx] as u16 * 5;
+        }
 
-            self.v[7] = 0; // Reset collision register
+        0x33 => { // Fx33 - LD B, Vx
+            mem.write_byte(self.idx, self.v[vx] / 100);
+            mem.write_byte(self.idx + 1, (self.v[vx] % 100) / 10);
+            mem.write_byte(self.idx + 2, self.v[vx] % 10);
+        }
 
-            for offset in 0..height {
-                let sprite_byte = mem.get_byte(self.idx + offset);
-                for bit in 0..8 {
-                    let pixel = (sprite_byte >> (7 - bit)) & 1 != 0;
-                    let x = (self.v[vx] as usize + bit) % DISPLAY_WIDTH;
-                    let y = (self.v[vy] as usize + offset as usize) % DISPLAY_HEIGHT;
-
-                    if self.display[x][y] & pixel {
-                        self.v[7] = 1; // Set collision flag
-                    }
-
-                    // XOR pixel
-                    self.display[x][y] ^= pixel;
-                }
+        0x55 => { // Fx55 - LD [I], Vx
+            for i in 0..vx {
+                mem.write_byte(self.idx, self.v[i]);
+                self.idx += 1;
             }
-            self.idx += height;
+            self.idx += 1;
         }
 
-        0xe => { // Starts with E
-            let vx = Chip8::get_vx(op_code);
-            if let Some(key) = KEYS.get_by_left(&self.v[vx]) {
-                match op_code & 0x00ff {
-                    0x9e => { // Ex9E - SKP Vx
-                        if window.is_key_down(*key) {
-                            self.pc += 2;
-                        }
-                    },
-                    0xa1 => { // ExA1 - SKNP Vx
-                        if !window.is_key_down(*key) {
-                            self.pc += 2;
-                        }
-                    },
-                    _ => ()
-                }
+        0x65 => { // Fx65 - LD Vx, [I]
+            for i in 0..vx {
+                self.v[i] = mem.get_byte(self.idx);
+                self.idx += 1;
             }
-        }
-
-        0xf => { // Starts with F
-            let vx = Chip8::get_vx(op_code);
-            match op_code & 0x00ff {
-                0x07 => { // Fx07 - LD Vx, DT
-                    self.v[vx] = self.dt;
-                }
-                0x0a => { // Fx0A - LD Vx, K
-                    let mut key_pressed = false;
-                    while !key_pressed {
-                        if let Some(key) = window.get_keys_pressed(KeyRepeat::No).get(0) {
-                            if let Some(chip8_key) = KEYS.get_by_right(key) {
-                                self.v[vx] = *chip8_key;
-                                key_pressed = true;
-                            }
-                        }
-                        window.update();
-                        std::thread::sleep(std::time::Duration::from_millis(5));
-                    }
-                }
-                0x15 => { // Fx15 - LD DT, Vx
-                    self.dt = self.v[vx];
-                }
-                0x18 => { // Fx18 - LD ST, Vx
-                    self.st = self.v[vx];
-                }
-                0x1e => { // Fx1E - ADD I, Vx
-                    self.idx += self.v[vx] as u16;
-                }
-                0x29 => { // Fx29 - LD F, Vx
-                    self.idx = self.v[vx] as u16 * 5;
-                }
-                0x33 => { // Fx33 - LD B, Vx
-                    mem.write_byte(self.idx, self.v[vx] / 100);
-                    mem.write_byte(self.idx + 1, (self.v[vx] % 100) / 10);
-                    mem.write_byte(self.idx + 2, self.v[vx] % 10);
-                }
-                0x55 => { // Fx55 - LD [I], Vx
-                    for i in 0..vx {
-                        mem.write_byte(self.idx, self.v[i]);
-                        self.idx += 1;
-                    }
-                    self.idx += 1;
-                }
-                0x65 => { // Fx65 - LD Vx, [I]
-                    for i in 0..vx {
-                        self.v[i] = mem.get_byte(self.idx);
-                        self.idx += 1;
-                    }
-                    self.idx += 1;
-                }
-                _ => ()
-            }
+            self.idx += 1;
         }
         _ => ()
     }
     self.pc += 2;
+}
+
+fn execute( &mut self, op_code: u16, mem: &mut Memory, window: &mut Window) {
+    match op_code >> 12 {
+        0x0 => self.execute_0nnn(op_code),
+        0x1 => self.execute_1nnn(op_code),
+        0x2 => self.execute_2nnn(op_code),
+        0x3 => self.execute_3xkk(op_code),
+        0x4 => self.execute_4xkk(op_code),
+        0x5 => self.execute_5xy0(op_code),
+        0x6 => self.execute_6xkk(op_code),
+        0x7 => self.execute_7xkk(op_code),
+        0x8 => self.execute_8nnn(op_code),
+        0x9 => self.execute_9xy0(op_code),
+        0xA => self.execute_Annn(op_code),
+        0xB => self.execute_Bnnn(op_code),
+        0xC => self.execute_Cxkk(op_code),
+        0xD => self.execute_Dxyn(op_code, &mem),
+        0xE => self.execute_Ennn(op_code, &window),
+        0xF => self.execute(op_code, mem, window),
+        _ => ()
+    }
 }
 
 pub fn run( &mut self, mem: &mut Memory ) {
@@ -332,7 +369,7 @@ pub fn run( &mut self, mem: &mut Memory ) {
         if instruction == 0x0fff { // HALT
             return;
         }
-        
+
         self.execute(instruction, mem, &mut display_ctl.window);
 
         std::thread::sleep(std::time::Duration::from_millis(MS_DELAY)); // delay
--- src/chip8/memory.rs
+++ src/chip8/memory.rs
@@ -1,5 +1,6 @@
 use std::io::{BufRead, BufReader};
 use std::fs::File;
+mod sprites;
 
 pub struct Memory {
     memory: [u8; 1024 * 4]
@@ -9,104 +10,7 @@ impl Memory {
     pub fn new() -> Self {
         let mut memory: [u8; 1024 * 4] = [0; 1024 * 4];
 
-        // "0"
-        memory[0x00] = 0xf0;
-        memory[0x01] = 0x90;
-        memory[0x02] = 0x90;
-        memory[0x03] = 0x90;
-        memory[0x04] = 0xf0;
-        
-        // "1"
-        memory[0x05] = 0x20;
-        memory[0x06] = 0x60;
-        memory[0x07] = 0x20;
-        memory[0x08] = 0x20;
-        memory[0x09] = 0x70;
-
-        // "2"
-        memory[0x0a] = 0xf0;
-        memory[0x0b] = 0x10;
-        memory[0x0c] = 0xf0;
-        memory[0x0d] = 0x80;
-        memory[0x0e] = 0xf0;
-        // "3"
-        memory[0x0f] = 0xf0;
-        memory[0x10] = 0x10;
-        memory[0x11] = 0xf0;
-        memory[0x12] = 0x10;
-        memory[0x13] = 0xf0;
-        // "4"
-        memory[0x14] = 0x90;
-        memory[0x15] = 0x90;
-        memory[0x16] = 0xf0;
-        memory[0x17] = 0x10;
-        memory[0x18] = 0x10;
-        // "5"
-        memory[0x19] = 0xf0;
-        memory[0x1a] = 0x80;
-        memory[0x1b] = 0xf0;
-        memory[0x1c] = 0x10;
-        memory[0x1d] = 0xf0;
-        // "6"
-        memory[0x1e] = 0xf0;
-        memory[0x1f] = 0x80;
-        memory[0x20] = 0xf0;
-        memory[0x21] = 0x90;
-        memory[0x22] = 0xf0;
-        // "7"
-        memory[0x23] = 0xf0;
-        memory[0x24] = 0x10;
-        memory[0x25] = 0x20;
-        memory[0x26] = 0x40;
-        memory[0x27] = 0x40;
-        // "8"
-        memory[0x28] = 0xf0;
-        memory[0x29] = 0x90;
-        memory[0x2a] = 0xf0;
-        memory[0x2b] = 0x90;
-        memory[0x2c] = 0xf0;
-        // "9"
-        memory[0x2d] = 0xf0;
-        memory[0x2e] = 0x90;
-        memory[0x2f] = 0xf0;
-        memory[0x30] = 0x10;
-        memory[0x31] = 0xf0;
-        // "A"
-        memory[0x32] = 0xf0;
-        memory[0x33] = 0x90;
-        memory[0x34] = 0xf0;
-        memory[0x35] = 0x90;
-        memory[0x36] = 0x90;
-        // "B"
-        memory[0x37] = 0xe0;
-        memory[0x38] = 0x90;
-        memory[0x39] = 0xe0;
-        memory[0x3a] = 0x90;
-        memory[0x3b] = 0xe0;
-        // "C"
-        memory[0x3c] = 0xf0;
-        memory[0x3d] = 0x80;
-        memory[0x3e] = 0x80;
-        memory[0x3f] = 0x80;
-        memory[0x40] = 0xf0;
-        // "D"
-        memory[0x41] = 0xe0;
-        memory[0x42] = 0x90;
-        memory[0x43] = 0x90;
-        memory[0x44] = 0x90;
-        memory[0x45] = 0xe0;
-        // "E"
-        memory[0x46] = 0xf0;
-        memory[0x47] = 0x80;
-        memory[0x48] = 0xf0;
-        memory[0x49] = 0x80;
-        memory[0x4a] = 0xf0;
-        // "F"
-        memory[0x4b] = 0xf0;
-        memory[0x4c] = 0x80;
-        memory[0x4d] = 0xf0;
-        memory[0x4e] = 0x80;
-        memory[0x4f] = 0x80;
+        sprites::set_character_sprites(&mut memory);
 
         Memory {
             memory
@@ -123,7 +27,7 @@ impl Memory {
 
     pub fn write_byte(&mut self, addr: u16, data: u8) {
         if addr < 0x200 {
-            panic!("Can't write to memory addresses 0x00 - 0x200");
+            panic!("Can't write to memory addresses 0x000 - 0x200");
         }
         self.memory[addr as usize] = data;
     }
@@ -133,33 +37,35 @@ impl Memory {
         self.write_byte(addr + 1, (data & 0xff) as u8);
     }
 
+    // Loading memory from file, pairs "addr data" need to be written line by line in hex, seperated by space
     pub fn from(file_path: &str) -> Result<Self, std::io::Error> {
         let file = File::open(file_path)?;
         let reader = BufReader::new(file);
         let mut memory = Self::new();
 
-        for line in reader.lines() {
+        for (i, line) in reader.lines().enumerate() {
             let line = line?;
             let mut split = line.split_whitespace();
 
+            // Get addr and data
             let addr = split.next().ok_or_else(|| std::io::Error::new(
                 std::io::ErrorKind::InvalidData,
-                "Missing address in file"
+                format!("Missing address in line {}", i+1)
             ))?;
             let data = split.next().ok_or_else(|| std::io::Error::new(
                 std::io::ErrorKind::InvalidData,
-                "Missing data in file"
+                format!("Missing data in line {}", i+1)
             ))?;
 
+            // Convert values to u16 and u8
             let addr = u16::from_str_radix(addr.trim_start_matches("0x"), 16).map_err(|e| {
-                println!("Failed to parse address: {}", addr); // Debug print
                 std::io::Error::new(std::io::ErrorKind::InvalidData, e.to_string())
             })?;
             let data = u8::from_str_radix(data.trim_start_matches("0x"), 16).map_err(|e| {
-                println!("Failed to parse data: {}", data); // Debug print
                 std::io::Error::new(std::io::ErrorKind::InvalidData, e.to_string())
             })?;
 
+            // Write byte to memory
             memory.write_byte(addr, data);
         }
 
